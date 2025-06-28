6f649c56ce4462acf3c05ca31b46dfe3036ebdbc
= <=>
2025-06-28 12:18:36 -0300

    first commit

---
+++ prisma/migrations/20250624230850_init/migration.sql
@@ -0,0 +1,13 @@
+
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
---
+++ src/common/index.ts
@@ -0,0 +1,1 @@
+export * from '../common/dtos/pagination.dto';
