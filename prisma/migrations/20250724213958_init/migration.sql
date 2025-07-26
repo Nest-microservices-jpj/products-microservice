b067e2ffce0474bf8c8355499c8bf771c576d933
= <=>
2025-07-26 11:13:58 -0300

    docker config

--- prisma/migrations/20250724213958_init/migration.sql
+++ prisma/migrations/20250724213958_init/migration.sql
@@ -1,13 +1,12 @@
-
-PRAGMA defer_foreign_keys=ON;
-PRAGMA foreign_keys=OFF;
+-- CreateTable
 CREATE TABLE "Product" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "price" REAL NOT NULL,
+    "available" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-PRAGMA foreign_keys=ON;
-PRAGMA defer_foreign_keys=OFF;
+-- CreateIndex
+CREATE INDEX "Product_available_idx" ON "Product"("available");
